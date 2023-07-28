d9f8ba94c8776fa354537bd0ab6671eed891461c
unknown <sanifmujawar@gmail.com>
2023-07-28 19:00:39 +0100

    Added questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+const questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      answers: ["strings", "booleans", "alerts", "numbers"],
+      correct: 2,
+    },
+    {
+      question:
+        "The condition in an if/else statement is enclosed within _______.",
+      answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+      correct: 2,
+    },
+    {
+      question: "Arrays in JavaScript can be used to store:",
+      answers: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above",
+      ],
+      correct: 3,
+    },
+    {
+      question:
+        "String values must be enclosed within ______ when being assigned to variables.",
+      answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+      correct: 2,
+    },
+    {
+      question:
+        "A very useful tool used during development and debugging for printing content to the debugger is",
+      answers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      correct: 3,
+    },
+  ];
+  
