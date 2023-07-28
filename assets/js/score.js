37c8fd0fb71e18b96ddffc77ea7129e1aac0c3f5
unknown <sanifmujawar@gmail.com>
2023-07-28 19:37:50 +0100

    Replaced for loop with a forEach loop

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,10 +1,11 @@
 const selectOlist = document.querySelector("ol");
 let highscore = JSON.parse(localStorage.getItem("scores")) || [];
-for (let i = 0; i < highscore.length; i++) {
+
+highscore.forEach((score) => {
   let li = document.createElement("li");
   selectOlist.appendChild(li);
-  li.textContent = `${highscore[i].initial} (${highscore[i].score}) points`;
-}
+  li.textContent = `${score.initial} (${score.score}) points`;
+});
 
 let clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () => {
